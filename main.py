32c0e77e77da01223c1dacc774d4fc3835226294
ioRM-b <ramosiovanni@gmail.com>
2025-03-26 08:30:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Ramos Morlet Iovanni", "+524756987", "Josue Perez", "+456789123"})
 
 
 if __name__ == '__main__':
